1814e90feaeecf0bdef42d9c8c16ddc52516b616
Lucas de Alencar Vieira da Silva <lucas.alencar31@gmail.com>
2024-04-24 16:22:44 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+const content = document.querySelector(".content");
+
+const test = document.createElement("h1");
+test.textContent = "This is a test";
+
+content.appendChild(test);
+
