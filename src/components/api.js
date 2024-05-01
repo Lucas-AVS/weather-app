ce2005f0c81454573f54b0646a7665e6cc712519
Lucas-AVS <lucas.alencar31@gmail.com>
2024-05-01 16:12:53 -0300

    feat: Fetch forecast data for weekly forecast display and refactor original fetch data function

--- src/components/api.js
+++ src/components/api.js
@@ -5,14 +5,28 @@ const currentLocation = "brasília";
 
 export async function api() {
   try {
-    const response = await fetch(
+    const fetchCurrent = fetch(
       `http://api.weatherapi.com/v1/current.json?key=${key}&q=${currentLocation}&aqi=yes`
     );
-    const data = await response.json();
-    const weatherCondition = data.current.condition.text;
-    return { data, weatherCondition };
+    const fetchForecast = fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${currentLocation}&days=7`
+    );
+
+    const [currentResponse, forecastResponse] = await Promise.all([
+      fetchCurrent,
+      fetchForecast,
+    ]);
+
+    const currentData = await currentResponse.json();
+    const forecastData = await forecastResponse.json();
+
+    const currentWeatherCondition = currentData.current.condition.text;
+    const forecastDays = forecastData.forecast.forecastday;
+
+    return { currentData, currentWeatherCondition, forecastData, forecastDays };
   } catch (error) {
     console.error("Error:", error);
+    // Handle the error based on your application's specific requirements
     throw error;
   }
 }
